baaaff6ec5ef485169c2f3353cde1e090e6cfde2
stefko-tsonyovski-fntext <stefko.tsonyovski.ext@fntsoftware.com>
2024-08-02 17:49:09 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import SignInForm from "./components/SignInForm";
+import { helperFunc } from "./utils";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <SignInForm />;
+};
 
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,1 @@
+export const helperFunc = (a, b) => a + b;
